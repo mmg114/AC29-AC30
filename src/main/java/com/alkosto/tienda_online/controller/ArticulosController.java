6717bed3df8760e4860ea3cf8e8f88164ffceb18
Mauricio-Munoz <mauricio.m@moabits.com>
2025-03-31 21:04:42 -0500

    config database

--- src/main/java/com/alkosto/tienda_online/controller/ArticulosController.java
+++ src/main/java/com/alkosto/tienda_online/controller/ArticulosController.java
@@ -1,7 +1,13 @@
 package com.alkosto.tienda_online.controller;
 
 
+import com.alkosto.tienda_online.domain.Articulo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.net.http.HttpResponse;
 
 @RestController
 @RequestMapping("/api/v1/articulo")
@@ -10,6 +16,16 @@ public class ArticulosController {
     @GetMapping
     public String getArticulo(){
         return "get Articulo";
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Articulo> getArticuloById(@PathVariable int id){
+        Articulo articulo = new Articulo();
+        articulo.setId(123123);
+        articulo.setNombre("Articulo");
+        articulo.setDescripcion("Articulo");
+        articulo.setPrecio(new BigDecimal(10000));
+        return new ResponseEntity<>(articulo,HttpStatus.ACCEPTED);
     }
 
     @PutMapping
---
+++ src/main/java/com/alkosto/tienda_online/domain/Articulo.java
@@ -0,0 +1,52 @@
+package com.alkosto.tienda_online.domain;
+
+import java.math.BigDecimal;
+
+
+public class Articulo {
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+    private String referencia;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+}
